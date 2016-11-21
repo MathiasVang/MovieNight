23ca346d9884496a5ad0ae296a4c5461033d1a44
Mathias Vang Rasmussen <mathiasvangrasmussen@Mikkels-iMac.local>
2016-11-21 11:19:30 +0100

    Initial Commit

---
+++ MovieNight/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieNight
+//
+//  Created by Mathias Vang Rasmussen on 21/11/2016.
+//  Copyright © 2016 ReCapted. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
